5a3b332fb40ad6de9867888dbd636cad1ed4b753
lxlcnfpc <lxl.cnfpc@pm.me>
2024-12-24 16:45:53 +0100

    Fix path resolution

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './src/index.css'  // Update path
-import App from './src/App.jsx'  // Update path
+import './index.css'  // Update path
+import App from './App.jsx'  // Update path
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
